650add255d09a5b63aa413cdf2c9834b343b63c7
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-07-28 18:35:04 -0400

    added source

---
+++ Terrain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain : MonoBehaviour
+{
+
+    [SerializeField] GameObject terrainTemplate;
+    int length = 10;
+
+    Vector3[] verticesTemp = new Vector3[4] {
+        new Vector3(0,0,0),
+        new Vector3(1,0,0),
+        new Vector3(0,0,1),
+        new Vector3(1,0,1)
+    };
+
+    void Start() {
+        for (int x = 0; x < length; x++) {
+            for (int z = 0; z < length; z++) {
+
+                GameObject terrain = Instantiate(terrainTemplate,new Vector3(x,0,z),Quaternion.identity);
+                terrain.GetComponent<Tile>().vertices = verticesTemp;
+                terrain.GetComponent<Tile>().RenderMesh();
+            }
+        }
+    }
+}
---
+++ Tile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Vector3[] vertices = new Vector3[4];
+
+    public void RenderMesh() {
+        MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+        MeshFilter filter = gameObject.AddComponent<MeshFilter>();
+
+        renderer.sharedMaterial = new Material(Shader.Find("Standard"));
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[6] {
+            0,2,1,
+            2,3,1
+        }; mesh.triangles = triangles;
+
+        mesh.normals = new Vector3[4] {
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward,
+        };
+
+        mesh.uv = new Vector2[4] {
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(0, 1),
+            new Vector2(1, 1)
+        };
+
+        filter.mesh = mesh;
+    }
+}
